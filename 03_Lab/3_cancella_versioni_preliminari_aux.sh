e8954b8056202c5c188034635f1c141a3a97d3e7
Michele Gabrieli <72445614+michele25-10@users.noreply.github.com>
2024-10-17 21:45:24 +0200

    Update 3_cancella_versioni_preliminari_aux.sh

--- 03_Lab/3_cancella_versioni_preliminari_aux.sh
+++ 03_Lab/3_cancella_versioni_preliminari_aux.sh
@@ -1,29 +1,29 @@
-#!/bin/bash sh 
+#!/bin/bash
 
-cd $1
+# Verifica che il primo argomento sia una directory valida
+cd "$1" || exit 1
 
 i=0
-echo $1
+echo "$1"
 
 for f in *; do
-    if test "$f" = *txt; then
-        if (test -w "$f"); then
-        echo $f sono dentro
-        version="$(head $f -n 1 | cut -d ',' -f3)"
-        if ( test $version = "preliminare"); then
-            i=$(expr $i + 1)
-            #rm $f
-            row_max_counter="$(cat $FATHER_PATH/max_counter.tmp)"
-            if (test $i -gt $row_max_counter); then
-                echo "i >  $row_max_counter"
-                echo "$i" > "$FATHER_PATH/max_counter.tmp"
-                echo "$1" > "$FATHER_PATH/max_dirname.tmp"
+    if [[ "$f" == *.txt ]]; then
+        if [[ -w "$f" ]]; then
+            echo "$f sono dentro"
+            version="$(head -n 1 "$f" | cut -d ',' -f3)"
+            
+            if [[ "$version" == "preliminare" ]]; then
+                i=$((i + 1))  # Incrementa i
+                row_max_counter="$(cat "$FATHER_PATH/max_counter.tmp")"
+                
+                if [[ $i -gt $row_max_counter ]]; then
+                    echo "i >  $row_max_counter"
+                    echo "$i" > "$FATHER_PATH/max_counter.tmp"
+                    echo "$1" > "$FATHER_PATH/max_dirname.tmp"
+                fi
             fi
         fi
-        fi
-    elif test -d $f; then
-        sh "$FATHER_PATH/3_cancella_versioni_preliminari_aux.sh" "$(pwd)/$f"
-    else
-    echo elseeeee
+    elif [[ -d "$f" ]]; then
+        bash "$FATHER_PATH/3_cancella_versioni_preliminari_aux.sh" "$(pwd)/$f"
     fi
 done
