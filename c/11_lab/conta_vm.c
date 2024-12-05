7191573b0afac1f2e12e28e5d365bf6e439f08af
michele25-10 <michele.gabri01@gmail.com>
2024-12-05 11:16:14 +0100

    Esercizio2 lab 11

--- c/11_lab/conta_vm.c
+++ c/11_lab/conta_vm.c
@@ -9,6 +9,7 @@
 #include <dirent.h>
 
 #define DIM 32
+#define DIM_BUFFER 16
 
 void ctrlc(int signo){
     printf( "\nProgramma terminato"); 
@@ -16,11 +17,10 @@ void ctrlc(int signo){
 }
 
 int main(int argc, char **argv){
-    char cliente[DIM]; 
-    char data[5]; 
-    int n; 
-    int p1p2[2]; int p2p3[2]; 
-    int pid1, pid2, pid3; 
+    char operatore[DIM]; 
+    char nome_app[DIM]; 
+    int p1p2[2]; int p2p0[2]; 
+    int pid1, pid2;  
     int status; 
     
     if(argc != 2){
@@ -33,39 +33,36 @@ int main(int argc, char **argv){
         exit(2);  
     }
 
-    DIR* dir = opendir(argv[1]);
-    if(dir == NULL){
-        fprintf(stderr, "\nLa directory non è assoluta\n"); 
+    int fd = open(argv[1], __O_DIRECTORY); 
+    if(fd < 0){
+        perror("Directory inesistente"); 
         exit(1); 
     } 
-    //fclose(dir); 
+    close(fd); 
 
     //Gestione del segnale
     signal(SIGINT, ctrlc); 
 
     while(1){
-        printf("\nInserisci il codice del cliente: "); 
-        scanf("%s", cliente); 
+        printf("\nInserisci il nome dell'operatore: "); 
+        scanf("%s", operatore); 
 
-        printf("\nInserisci la data di interesse (MMGG): "); 
-        scanf("%s", data); 
-
-        printf("\nInserisci il numero di righe di interesse: "); 
-        scanf("%d", &n);
+        printf("\nInserisci il nome dell'app: "); 
+        scanf("%s", nome_app); 
 
         if(pipe(p1p2) < 0){
             perror("Errore creazione pipe p1p2"); 
             exit(3); 
         }
 
-        if(pipe(p2p3) < 0){
+        if(pipe(p2p0) < 0){
             perror("Errore creazione pipe p2p3"); 
             exit(4); 
         }
 
         //Controllo esistenza del file
         char filePath[128]; 
-        sprintf(filePath, "%s/%s.tran", argv[1], data); 
+        sprintf(filePath, "%s/%s.txt", argv[1], operatore); 
         int fd = open(filePath, O_RDONLY); 
         if(fd < 0){
             perror("File inesistente errore apertura file"); 
@@ -82,7 +79,7 @@ int main(int argc, char **argv){
             dup(p1p2[1]); 
             close(p1p2[1]); 
 
-            execlp("grep", "grep", cliente, filePath, (char*)0); 
+            execlp("grep", "grep", nome_app, filePath, (char*)0); 
             perror("Errore execlp"); 
 
             exit(10); 
@@ -91,6 +88,8 @@ int main(int argc, char **argv){
             perror("Errore creazione pid1"); 
             exit(11); 
         }
+
+        close(p1p2[1]); 
 
         pid2 = fork(); 
         if(pid2 == 0){
@@ -102,13 +101,13 @@ int main(int argc, char **argv){
             dup(p1p2[0]); 
             close(p1p2[0]); 
 
-            close(p2p3[0]); 
+            close(p2p0[0]); 
 
             close(1); 
-            dup(p2p3[1]); 
-            close(p2p3[1]); 
+            dup(p2p0[1]); 
+            close(p2p0[1]); 
 
-            execlp("sort", "sort", "-n", (char*)0); 
+            execlp("grep", "grep", "-c", "operativa", (char*)0); 
             perror("Errore execlp"); 
 
             exit(11); 
@@ -120,46 +119,18 @@ int main(int argc, char **argv){
 
         //chiudo le pipe che non servono più a pid3
         close(p1p2[0]); 
-        close(p1p2[1]); 
+        close(p2p0[1]); 
 
-        pid3 = fork(); 
-        if(pid3 == 0){
-            signal(SIGINT, SIG_DFL); 
-
-            close(p2p3[1]); 
-            
-            close(0); 
-            dup(p2p3[0]); 
-            close(p2p3[0]); 
-
-            close(1); 
-            //apro il file
-            int fd = open("importi_massimi.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666); 
-            if(fd < 0){
-                perror("Errore creazione file"); 
-                exit(11); 
-            }
-
-            char righe[4]; 
-            sprintf(righe, "%d", n);  
-
-            execlp("head", "head", "-n", righe, (char*)0); 
-            perror("Errore execlp"); 
-
-            exit(12); 
-        }   
-        if(pid3 < 0){
-            perror("Errore creazione pid3"); 
-            exit(11); 
-        }
-        
-        close(p2p3[0]); 
-        close(p2p3[1]); 
+        char str[DIM_BUFFER]; 
+        int n_read = read(p2p0[0], str, DIM_BUFFER-1);
+        str[n_read] = '\0'; 
+        close(p2p0[0]); 
+        printf("%s\n", str);
 
         wait(&status); 
         wait(&status); 
-        wait(&status); 
-
     }
+
+    return 0; 
 
 }
